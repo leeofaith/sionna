88a777040ba36c9efae4581f682e0f3d0a3e9de8
leeofaith <leeofaith@gmail.com>
2023-05-22 16:18:18 +1000

    Push

---
+++ Jupyter Notebooks/UncodedSystemFlatFading.py
@@ -0,0 +1,215 @@
+import tensorflow as tf
+# For the implementation of the Keras models
+from tensorflow import keras
+from keras import Model
+# for performance measurements
+import time
+
+# GPU Configuration and Imports
+# Configure the notebook to use only a single GPU and allocate only as much memory as needed
+# For more details, see https://www.tensorflow.org/guide/gpu
+gpus = tf.config.list_physical_devices('GPU')
+print('Number of GPUs available :', len(gpus))
+if gpus:
+    gpu_num = 0 # Number of the GPU to be used
+    try:
+        tf.config.set_visible_devices(gpus[gpu_num], 'GPU')
+        print('Only GPU number', gpu_num, 'used.')
+        tf.config.experimental.set_memory_growth(gpus[gpu_num], True)
+    except RuntimeError as e:
+        print(e)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+# Import Sionna
+try:
+    import sionna as sn
+except ImportError as e:
+    # Install Sionna if package is not already installed
+    import os
+    os.system("pip install sionna")
+    import sionna as sn
+
+from xyDIP import DeepImagePrior
+from complex2real import Complex2Real
+
+class UncodedSystemFlatFading(Model): # Inherits from Keras Model
+    def __init__(self,
+                 Block_Length,
+                 NUM_BITS_PER_SYMBOL,
+                 CONSTELLATION_TYPE,
+                 DEMAPPING_METHOD,
+                 NUM_TX_ANT,
+                 NUM_RX_ANT):
+        """
+        A keras model of an uncoded transmission over the AWGN channel.
+
+        Parameters
+        ----------
+        NUM_BITS_PER_SYMBOL: int
+            The number of bits per constellation symbol, e.g., 4 for QAM16.
+
+        Block_Length: int
+            The number of bits per transmitted message block (will be the codeword length later).
+
+        Input
+        -----
+        BATCH_SIZE: int
+            The BATCH_SIZE of the Monte-Carlo simulation.
+
+        EBN0_DB: float
+            The `Eb/No` value (=rate-adjusted SNR) in dB.
+
+        Output
+        ------
+        (bits, llr):
+            Tuple:
+
+        bits: tf.float32
+            A tensor of shape `[BATCH_SIZE, Block_Length] of 0s and 1s
+            containing the transmitted information bits.
+
+        llr: tf.float32
+            A tensor of shape `[BATCH_SIZE, Block_Length] containing the
+            received log-likelihood-ratio (LLR) values.
+        """
+
+        super().__init__() # Must call the Keras model initializer
+        
+        self.Block_Length = Block_Length
+        self.NUM_BITS_PER_SYMBOL = NUM_BITS_PER_SYMBOL
+        self.CONSTELLATION_TYPE = CONSTELLATION_TYPE
+        self.DEMAPPING_METHOD = DEMAPPING_METHOD
+        self.NUM_TX_ANT = NUM_TX_ANT
+        self.NUM_RX_ANT = NUM_RX_ANT
+        self.constellation = sn.mapping.Constellation(constellation_type=self.CONSTELLATION_TYPE,
+                                                      num_bits_per_symbol=self.NUM_BITS_PER_SYMBOL)
+        self.mapper = sn.mapping.Mapper(constellation_type=self.CONSTELLATION_TYPE,
+                                        num_bits_per_symbol=self.NUM_BITS_PER_SYMBOL)
+        self.demapper = sn.mapping.Demapper(demapping_method=self.DEMAPPING_METHOD,
+                                            constellation_type=self.CONSTELLATION_TYPE,
+                                            num_bits_per_symbol=self.NUM_BITS_PER_SYMBOL)
+        self.SymbolDemapper = sn.mapping.SymbolDemapper(constellation_type=self.CONSTELLATION_TYPE,
+                                                        num_bits_per_symbol=self.NUM_BITS_PER_SYMBOL,
+                                                        hard_out=True)
+        # self.LDPC5GEncoder = sn.fec.ldpc.encoding.LDPC5GEncoder()
+        # self.LDPC5GDecoder = sn.fec.ldpc.decoding.LDPC5GDecoder()
+        self.binary_source = sn.utils.BinarySource()
+        self.awgn_channel = sn.channel.AWGN()
+        self.flatfading_channel = sn.channel.FlatFadingChannel(num_tx_ant=self.NUM_TX_ANT,
+                                                               num_rx_ant=self.NUM_TX_ANT,
+                                                               add_awgn=True,
+                                                               return_channel=True)
+        # self.KroneckerModel = sn.channel.KroneckerModel()
+        # self.exp_corr_mat = sn.channel.utils.exp_corr_mat()
+        # self.hard_decisions = sn.utils.misc.hard_decisions()
+        self.dip = DeepImagePrior(num_rx_ant=NUM_RX_ANT,
+                                  num_tx_ant=NUM_TX_ANT, # Number of transmitted symbol in real domain
+                                  M=2**NUM_BITS_PER_SYMBOL, # Modulation order (16), 4 for 4QAM, 16 for 16QAM(4 bits/symbol)
+                                  iteration=100, # Number of max iterations used for DIP: 100
+                                  LR=0.01, # Learning rate,  typically set to 0.01
+                                  buffer_size=30, # Iterations stored,  typically set to 30
+                                  threshold=0.001, # Threshold of DIP stop,, typically set to 0.001
+                                  stop=True) # True
+        self.c2r = Complex2Real(num_rx_ant=NUM_RX_ANT,num_tx_ant=NUM_TX_ANT)
+
+    # @tf.function # Enable graph execution to speed things up
+    def __call__(self, NUM_DATA_GROUP, BATCH_SIZE, EBN0_DB_MIN, EBN0_DB_MAX, NUM_EBN0_POINTS):
+
+        snrs = np.linspace(EBN0_DB_MIN,EBN0_DB_MAX,NUM_EBN0_POINTS)
+        # bers = []
+        sers_zf = np.empty((NUM_DATA_GROUP, NUM_EBN0_POINTS))
+        sers_lmmse = np.empty((NUM_DATA_GROUP, NUM_EBN0_POINTS))
+        sers_dip = np.empty((NUM_DATA_GROUP, NUM_EBN0_POINTS))
+
+        for i in range(0, NUM_DATA_GROUP):
+
+            print('Data Group {}'.format(i))
+            print('Processing...')
+            b = self.binary_source([BATCH_SIZE,self.NUM_TX_ANT,self.Block_Length])
+            x =  self.mapper(b)
+            shape = tf.shape(x)
+            x_reshape = tf.reshape(x,[-1, self.NUM_TX_ANT])
+            j = 0
+
+            for EBN0_DB in np.linspace(EBN0_DB_MIN,EBN0_DB_MAX,NUM_EBN0_POINTS):
+                # print('EBN0_DB =',EBN0_DB)
+                # no channel coding used; we set coderate=1.0
+                no = sn.utils.ebnodb2no(ebno_db=EBN0_DB,
+                                        num_bits_per_symbol=self.NUM_BITS_PER_SYMBOL,
+                                        coderate=1.0) # Coderate set to 1 as we do uncoded transmission here
+                x_ind = self.SymbolDemapper([x, no])
+
+                # y and h are the Channel Output and Channel Realizations, respectively
+                y, h = self.flatfading_channel([x_reshape, no])
+                s = tf.cast(no*tf.eye(self.NUM_RX_ANT, self.NUM_RX_ANT), y.dtype)
+                x_hat_zf, no_eff_zf = sn.mimo.zf_equalizer(y, h, s)
+                x_hat_lmmse, no_eff_lmmse = sn.mimo.lmmse_equalizer(y, h, s)
+                X_inCH_real,H_real,Y_real = self.c2r.C2R(x_reshape,h,y)
+                x_dip_ay,num_stop_point = self.dip.DIP(Y_real,H_real)
+                x_dip_ay_real_part,x_dip_ay_imag_part = tf.split(x_dip_ay, num_or_size_splits=2, axis=2)
+                
+                x_hat_dip = tf.squeeze(tf.squeeze(tf.complex(x_dip_ay_real_part,x_dip_ay_imag_part),axis=-1),axis=-1)
+                x_hat_zf = tf.reshape(x_hat_zf, shape)
+                x_hat_lmmse = tf.reshape(x_hat_lmmse, shape)
+                x_hat_dip = tf.reshape(x_hat_dip, shape)
+
+                # no_eff_zf = tf.reshape(no_eff_zf, shape)
+                # no_eff_lmmse = tf.reshape(no_eff_lmmse, shape)
+
+                # llr_zf = self.demapper([x_hat_zf, no_eff_zf])
+                # b_hat_zf = self.LDPC5GDecoder(llr_zf)
+
+                x_ind_hat_zf = self.SymbolDemapper([x_hat_zf, no])
+                x_ind_hat_lmmse = self.SymbolDemapper([x_hat_lmmse, no])
+                x_ind_hat_dip = self.SymbolDemapper([tf.cast(x_hat_dip, dtype=tf.complex64), no])
+
+                ser_zf = sn.utils.compute_ser(x_ind, x_ind_hat_zf)
+                ser_lmmse = sn.utils.compute_ser(x_ind, x_ind_hat_lmmse)
+                ser_dip = sn.utils.compute_ser(x_ind, x_ind_hat_dip)
+                sers_zf[i, j] = ser_zf
+                sers_lmmse[i, j] = ser_lmmse
+                sers_dip[i, j] = ser_dip
+                j = j+1
+            print('Done')
+
+        sers_zf_mean = np.mean(sers_zf, axis=0)
+        sers_lmmse_mean = np.mean(sers_lmmse, axis=0)
+        sers_dip_mean = np.mean(sers_dip, axis=0)
+
+        plt.figure(1)
+        plt.axes().set_aspect(1)
+        plt.grid(True)
+        plt.title('Flat-Fading Channel Constellation', fontsize=12)
+        plt.xticks(fontsize=10)
+        plt.yticks(fontsize=10)
+        plt.xlabel('REAL', fontsize=10)
+        plt.ylabel('IMAG', fontsize=10)
+        plt.scatter(tf.math.real(x), tf.math.imag(x), s=16, c='b', label='TX')
+        plt.scatter(tf.math.real(x_hat_zf), tf.math.imag(x_hat_zf), s=16, c='y', label='ZF')
+        plt.scatter(tf.math.real(x_hat_lmmse), tf.math.imag(x_hat_lmmse), s=16, c='g', label='LMMSE')
+        plt.scatter(tf.math.real(x_hat_dip), tf.math.imag(x_hat_dip), s=16, c='r', label='DIP')
+        plt.legend(loc='lower left', fontsize=8)
+        plt.tight_layout()
+
+        plt.figure(2)
+        title = "SER: Noncoding MIMO Falt-Fading with ZF, MMSE, DIP Equalizer"
+        xlabel = "$E_b/N_0$ (dB)"
+        ylabel = "SER (log)"
+        plt.title(title, fontsize=12)
+        plt.xticks(fontsize=10)
+        plt.yticks(fontsize=10)
+        plt.xlabel(xlabel, fontsize=10)
+        plt.ylabel(ylabel, fontsize=10)
+        plt.grid(which="both")
+        plt.semilogy(snrs, sers_zf_mean, 'b', label='ZF')
+        plt.semilogy(snrs, sers_lmmse_mean, 'g', label='LMMSE')
+        plt.semilogy(snrs, sers_dip_mean, 'r', label='DIP')
+        plt.legend(loc='lower left', fontsize=8)
+        plt.tight_layout()
+
+        plt.show()
+
+        return snrs, sers_zf_mean, sers_lmmse_mean, sers_dip_mean
--- Jupyter Notebooks/xyDIP copy.py
+++ Jupyter Notebooks/xyDIP copy.py
@@ -2,19 +2,17 @@ import numpy as np
 import torch
 from torch import nn
 
-
-
 class DeepImagePrior(object):
     def __init__(self,num_rx_ant,num_tx_ant,M, iteration,LR,buffer_size,threshold,stop):
         
         self.num_rx_ant = num_rx_ant
-        self.num_tx_ant = num_tx_ant    ####number of transmitted symbol in real domain;
-        self.M = M                  ####modulation order, 4 for 4qam, 16 for 16qam
-        self.iteration = iteration  ####number of max iterations used for DIP
-        self.LR = LR                ####Learning rate,  typically set to 0.01; Control step size of updating the model parameters at each iteration
-        self.buffer_size = buffer_size    ###iterations stored,  typically set to 30
-        self.threshold = threshold        ###Threshold of DIP stop,, typically set to 0.001
-        self.stop = stop                  ###True
+        self.num_tx_ant = num_tx_ant      #### Number of transmitted symbols in real domain;
+        self.M = M                        #### Modulation order, 4 for 4QAM, 16 for 16QAM;
+        self.iteration = iteration        #### Number of max iterations used for DIP;
+        self.LR = LR                      #### Learning rate, typically set to 0.01; Control step size of updating the model parameters at each iteration;
+        self.buffer_size = buffer_size    #### Iterations stored,  typically set to 30;
+        self.threshold = threshold        #### Threshold of DIP stop,, typically set to 0.001;
+        self.stop = stop                  #### True;
         constellation = np.linspace(int(-np.sqrt(M) + 1), int(np.sqrt(M) - 1), int(np.sqrt(M)))
         alpha = np.sqrt((constellation ** 2).mean())
         constellation /= (alpha * np.sqrt(2))
@@ -62,18 +60,18 @@ class DeepImagePrior(object):
         x_dist_true = x_real_true + x_imag_true
         true_indices = np.argmin(x_dist_true, axis=-1)
         
-         # estim_indices = joint_indices(x_hat_indices,constellation)
+        # estim_indices = joint_indices(x_hat_indices,constellation)
         ser = np.sum(true_indices!=estim_indices)/true_indices.size
         return ser
 
 
     def DIP(self,Y,H):
         
-        torch.backends.cudnn.enabled = True # Enable CUDA DNN library (cuDNN) to accelerate NN operations
-        torch.backends.cudnn.benchmark = True # Find the most suitable cuDNN algorithm to maximize performance
+        torch.backends.cudnn.enabled = True ### Enable CUDA DNN library (cuDNN) to accelerate NN operations
+        torch.backends.cudnn.benchmark = True ### Find the most suitable cuDNN algorithm to maximize performance
         dtype = torch.FloatTensor
         batch_size = H.shape[0]
-        x_dip_ay = np.empty((batch_size,2*self.num_tx_ant))
+        x_dip_ay = np.empty((batch_size,self.num_tx_ant,2,1))
         num_stop_point = []
         
         for bs in range(batch_size):
@@ -81,20 +79,23 @@ class DeepImagePrior(object):
             i = 0            
             flag = False
             
-            net = Decoder(self.num_tx_ant).type(dtype)    ### Define the Neural network
-            mse = torch.nn.MSELoss().type(dtype)        ### Loss function: Mean Squared Error; MSE = \frac{1}{n}\sum_{i=1}^{n}(y_i - \hat{y}_i)^2
-            # Instance Adam Optimizer
-            optimizer = torch.optim.Adam(net.parameters(), lr= self.LR)     ###Adam optimizer
-            # Reset the Weight of the convolutional or linear layers to random values
-            net.apply(weight_reset)                     ### Reset the neural network parameters
-            # 正态分布数组size(1,4),可以改成均匀分布等其他分布
-            net_input = torch.randn(1,4)    ### Random input for DIP
+            net = Decoder(self.num_tx_ant,self.num_tx_ant).type(dtype) ### Define the Neural network
+            mse = torch.nn.MSELoss().type(dtype) ### Loss function: Mean Squared Error; MSE = \frac{1}{n}\sum_{i=1}^{n}(y_i - \hat{y}_i)^2
+            ### Instance Adam Optimizer
+            optimizer = torch.optim.Adam(net.parameters(), lr= self.LR) ###Adam optimizer
+            ### Reset the Weight of the convolutional or linear layers to random values
+            net.apply(weight_reset) ### Reset the neural network parameters
+            ### Normal distribution arry with size(1,4), can be modified to other distribuion types
+            net_input = torch.randn(1,4) ### Random input for DIP
             # print('Y shape=',Y.shape)
             # print('Y =',Y)
             # print('H shape=',H.shape)
             # print('H =',H)
-            y_torch = torch.from_numpy((Y[bs]).numpy()).reshape(1,1,2*self.num_rx_ant,1).type(dtype)
-            H_torch = torch.from_numpy((H[bs]).numpy()).reshape(1,1,2*self.num_rx_ant,2*self.num_tx_ant).type(dtype)
+            # y_torch = torch.from_numpy((Y[bs]).numpy()).reshape(1,1,2*self.num_rx_ant,1).type(dtype)
+            # H_torch = torch.from_numpy((H[bs]).numpy()).reshape(1,1,2*self.num_rx_ant,2*self.num_tx_ant).type(dtype)
+
+            y_torch = torch.from_numpy((Y[bs]).numpy()).type(dtype)
+            H_torch = torch.from_numpy((H[bs]).numpy()).type(dtype)
             # print('y_torch shape=',y_torch.shape)
             # print('y_torch =',y_torch)
             # print('H_torch shape=',H_torch.shape)
@@ -107,8 +108,8 @@ class DeepImagePrior(object):
                 
                 i+=1 
                 
-                # step 1, update network:
-                # Zeroize Gradient
+                ### step 1, update network:
+                ### 1) Zeroize Gradient
                 optimizer.zero_grad()
                 net_output = net(net_input).type(dtype)
                 # print('net_output shape =', net_output.shape)
@@ -119,15 +120,18 @@ class DeepImagePrior(object):
                 out = net_output*max_constellation
                 # print('out =', out)
                 # out = net(net_input).type(dtype)*np.max(self.constellation)
-                out1 = out.reshape(1,1,2*self.num_tx_ant,1).type(dtype)
                 # print('out1 shape =',out1.shape)
                 # print('out1 =',out1)
-
-                # result = torch.matmul(H_torch,out1).type(dtype)
+                out1 = torch.reshape(out.repeat(self.num_rx_ant,1,1,1),[self.num_rx_ant,self.num_tx_ant,2,1])
+                # print('out1 shape =',out1.shape)
+                # print('out1 =',out1)
+                result = torch.matmul(H_torch,out1)
+                # print('result shape =',result.shape)
                 # print('result =',result)
-                # Y_hat = torch.sum(result, axis=2)
-
-                Y_hat = torch.matmul(H_torch,out1).type(dtype)
+                Y_hat = torch.sum(result, axis=1)
+                # print('Y_hat shape =',Y_hat.shape)
+                # print('Y_hat =',Y_hat)
+                # Y_hat = torch.matmul(H_torch,out1).type(dtype)
 
                 # print('Y_hat =',Y_hat)
                 # print('y_torch =',y_torch)
@@ -148,7 +152,7 @@ class DeepImagePrior(object):
                     # print('img_collection[0] =', img_collection[0])
                     # print('img_collection =', img_collection)
                     if len(img_collection) ==  self.buffer_size:
-                        ave_img = np.mean(img_collection, axis=0) # Average buffer_sized img_colletction
+                        ave_img = np.mean(img_collection, axis=0) ### Average buffer_sized img_colletction
                         # print('ave_img =', ave_img)
                         # print('ave_img length =', len(ave_img))
                         variance = []
@@ -165,27 +169,32 @@ class DeepImagePrior(object):
                         num_stop_point.append(i)
                         flag = True
 
-            print('out1 =',out1)
+            # H_torch_qualified = H_torch.reshape(self.num_rx_ant,self.num_tx_ant,2,2)
+            # out1 = out1.reshape(self.num_rx_ant,self.num_tx_ant,2,2)
+            # print('out1[1] shape=',out1[1].shape)
+            # print('out1[1] =',out1[1])
+            # print('H_torch shape=',H_torch.shape)
             # print('H_torch =',H_torch)
             # print('total_loss =',total_loss)
+            # print('Y_hat shape=',Y_hat.shape)
             # print('Y_hat =',Y_hat)
             # print('y_torch =',y_torch)            
             # print('x_dip =', x_dip)            
-            x_dip_ay[bs] = x_dip.reshape(-1)
+            x_dip_ay[bs] = out1[1].detach().numpy()
             # print('x_dip_ay[{}] ={}'.format(bs, x_dip_ay[bs]))
         return x_dip_ay,num_stop_point
 
 class Decoder(nn.Module):
-    def __init__(self,num_tx_ant):
+    def __init__(self,num_rx_ant,num_tx_ant):
         super().__init__()
         
-        self.nn1 = nn.Linear(4,8) # Define numbers of input (4) and output (8) of fully connected layer
-                                  # Weight.shape = (number of output (8),number of input (4))
-                                  # Bias.shape = number of output (8)
+        self.nn1 = nn.Linear(4,8) ### Define numbers of input (4) and output (8) of fully connected layer
+                                  ### Weight.shape = (number of output (8),number of input (4))
+                                  ### Bias.shape = number of output (8)
         self.nn2 = nn.Linear(8,16)
         self.nn3 = nn.Linear(16,32)
-        self.nn4 = nn.Linear(32,(2*num_tx_ant)) # Tx
-        self.act = nn.Tanh() # Define Activation Function: Tanh()
+        self.nn4 = nn.Linear(32,(2*num_tx_ant))
+        self.act = nn.Tanh() ### Define Activation Function: Tanh()
          
     def forward(self,x): 
         o1 = self.act(self.nn1(x)) 
@@ -201,15 +210,15 @@ class EarlyStop():
         self.size = size
 
     def update_img_collection(self, cur_img):
-        self.img_collection.append(cur_img) # Adds an element to the end of the List
+        self.img_collection.append(cur_img) ### Adds an element to the end of the List
         # print('self.img_collection =', self.img_collection)
         # print('self.img_collection length =', len(self.img_collection))
         # print('self.img_collection[0] =', self.img_collection[0])
         if len(self.img_collection) > self.size:
-            self.img_collection.pop(0) # Removes img_collection[0]
+            self.img_collection.pop(0) ### Removes img_collection[0]
     def get_img_collection(self):
         return self.img_collection
-    def Var_cal(self, x1, x2): # Add 'self'
+    def Var_cal(self, x1, x2): ### Add 'self'
         # print('x1 =', x1)
         # print('x2 =', x2)
         # a = x1-x2
@@ -225,7 +234,7 @@ class EarlyStop():
         # return e
         return ((x1 - x2) ** 2).sum() / x1.size
     
-# Reset the Weight of the convolutional or linear layers to random values
+### Reset the Weight of the convolutional or linear layers to random values
 def weight_reset(m):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
         m.reset_parameters() 
